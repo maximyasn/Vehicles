9af125547b38d9d8a41cc36c35fdf44a229f9ef8
maximyasn <maximyasniy11@gmail.com>
2022-05-17 21:03:53 +0300

    Add methods into classes from package "GroundTransport"

--- src/GroundTransport/Tank.java
+++ src/GroundTransport/Tank.java
@@ -1,4 +1,10 @@
 package GroundTransport;
+import Interfaces.BattleTransport;
 
-public class Tank {
+public class Tank implements BattleTransport {
+    String weapon = "Cannon";
+
+    public void WeaponType() {
+        System.out.println(weapon);
+    }
 }
